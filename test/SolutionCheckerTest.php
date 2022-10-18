8b2744e45568c12c7feafaa0c8d9a2f8187d6f1d
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-10-18 17:15:48 +0200

    Some tests with a fake implementation. Ready to start working.

---
+++ test/SolutionCheckerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sudoku;
+
+use PHPUnit\Framework\TestCase;
+
+class SolutionCheckerTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider matrixProvider
+     */
+    public function recognize_valid_matrices(array $matrix, bool $expectedResult): void
+    {
+        $solutionChecker = new SolutionChecker();
+        $this->assertEquals($expectedResult, $solutionChecker->isPossibleSolution($matrix));
+    }
+
+    public function matrixProvider(): array
+    {
+        return
+            [
+                [
+                    [
+                        [1, 2, 3, 4,],
+                        [2, 1, 4, 3,],
+                        [3, 4, 1, 2,],
+                        [4, 3, 2, 1,],
+                    ],
+                    false,
+                ],
+                [
+                    [
+                        [1, 2, 3, 4,],
+                        [2, 1, 4, 3,],
+                        [3, 4, 0, 2,],
+                        [4, 0, 2, 1,],
+                    ],
+                    false,
+                ],
+                [
+                    [
+                        [1, 2, 3, 4,],
+                        [3, 4, 1, 2,],
+                        [2, 3, 4, 1,],
+                        [4, 1, 2, 3,],
+                    ],
+                    true,
+                ]
+            ];
+    }
+}
