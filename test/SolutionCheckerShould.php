e0c0d2550532ddaace6a5403900ce6e9cc6df39f
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-05-24 19:50:12 +0200

    Test cases for every possible rule violation

--- test/SolutionCheckerShould.php
+++ test/SolutionCheckerShould.php
@@ -23,6 +23,10 @@ class SolutionCheckerShould extends TestCase
     {
         return [
             [ $this->buildInitialGrid(), $this->buildCorrectSolution(), true ],
+            [ $this->buildInitialGrid(), $this->buildIncompleteSolution(), false ],
+            [ $this->buildInitialGrid(), $this->buildRepeatedNumberInColumn(), false ],
+            [ $this->buildInitialGrid(), $this->buildRepeatedNumberInRow(), false ],
+            [ $this->buildInitialGrid(), $this->buildRepeatedNumberInQuadrant(), false ],
         ];
     }
 
@@ -53,6 +57,66 @@ class SolutionCheckerShould extends TestCase
             9,6,1,5,3,7,2,8,4,
             2,8,7,4,1,9,6,3,5,
             3,4,5,2,8,6,1,7,9,
+        ];
+    }
+
+    private function buildIncompleteSolution(): array
+    {
+        return [
+            5,3,4,6,7,8,9,1,2,
+            6,7,2,1,9,5,3,4,8,
+            1,9,8,3,4,2,5,6,7,
+            8,5,9,7,6,1,4,2,3,
+            4,2,6,8,5,3,7,9,1,
+            7,1,3,9,2,4,8,5,6,
+            9,6,1,5,3,7,2,8,4,
+            2,8,7,4,1,9,6,3,5,
+            3,4,5,2,8,6,1,7,0,
+        ];
+    }
+
+    private function buildRepeatedNumberInColumn(): array
+    {
+        return [
+            5,3,4,6,7,8,9,1,2,
+            6,7,2,1,9,5,3,4,8,
+            1,9,8,3,4,2,5,6,7,
+            8,5,9,7,6,1,4,2,3,
+            4,2,6,8,5,3,7,9,1,
+            7,1,3,9,2,4,8,5,6,
+            9,6,1,5,3,7,2,8,4,
+            2,8,7,4,1,9,6,3,5,
+            3,4,5,2,8,6,1,7,6,
+        ];
+    }
+
+    private function buildRepeatedNumberInRow(): array
+    {
+        return [
+            5,3,4,6,7,8,9,1,2,
+            6,7,2,1,9,5,3,4,8,
+            1,9,8,3,4,2,5,6,7,
+            8,5,9,7,6,1,4,2,3,
+            4,2,6,8,5,3,7,9,1,
+            7,1,3,9,2,4,8,5,6,
+            9,6,1,5,3,7,2,8,4,
+            2,8,7,4,1,9,6,3,5,
+            3,4,5,2,8,6,1,7,7,
+        ];
+    }
+
+    private function buildRepeatedNumberInQuadrant(): array
+    {
+        return [
+            5,3,4,6,7,8,9,1,2,
+            6,7,2,1,9,5,3,4,8,
+            1,9,8,3,4,2,5,6,7,
+            8,5,9,7,6,1,4,2,3,
+            4,2,6,8,5,3,7,9,1,
+            7,1,3,9,2,4,8,5,6,
+            9,6,1,5,3,7,2,8,4,
+            2,8,7,4,1,9,6,3,5,
+            3,4,5,2,8,6,1,7,3,
         ];
     }
 }
